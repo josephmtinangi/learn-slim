da94b26b69952bfbfb1ec64d633cccd36c6b11b0
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-09-08 17:08:34 +0300

    Tidy things

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controllers;
 
-use App\Models\User;
 use Psr\Http\Message\RequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Views\Twig;
@@ -11,10 +10,6 @@ class HomeController
 {
     public function index(Request $request, Response $response, Twig $view)
     {
-        $users = User::get();
-
-        return $view->render($response, 'home.twig', [
-            'users' => $users,
-        ]);
+        return $view->render($response, 'home.twig');
     }
 }
--- app/Controllers/ProjectController.php
+++ app/Controllers/ProjectController.php
@@ -5,24 +5,36 @@ namespace App\Controllers;
 use App\Models\Project;
 use Psr\Http\Message\RequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use Slim\Router;
 use Slim\Views\Twig;
 
 class ProjectController
 {
-    public function index(Request $request, Response $response, Twig $view, Project $project)
+    protected $view;
+
+    function __construct(Twig $view)
+    {
+        $this->view = $view;
+    }
+
+    public function index(Request $request, Response $response, Project $project)
     {
         $projects = Project::get();
 
-        return $view->render($response, 'projects/index.twig', [
+        return $this->view->render($response, 'projects/index.twig', [
             'projects' => $projects
         ]);
     }
 
-    public function show($id, Request $request, Response $response, Twig $view, Project $project)
+    public function show($id, Request $request, Response $response, Router $router, Project $project)
     {
         $project = Project::find($id);
 
-        return $view->render($response, 'projects/show.twig', [
+        if (!$project) {
+            return $response->withRedirect($router->pathFor('home'));
+        }
+
+        return $this->view->render($response, 'projects/show.twig', [
             'project' => $project
         ]);
     }
