87b3292985dd9f90357be95b424bed53488fcb68
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-09-08 14:50:50 +0300

    Setup eloquent

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,14 +2,19 @@
 
 namespace App\Controllers;
 
+use App\Models\User;
 use Psr\Http\Message\RequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Views\Twig;
 
 class HomeController
 {
-    public function index(Request $request, Response $response, Twig $view)
+    public function index(Request $request, Response $response, Twig $view, User $user)
     {
+        $users = User::all();
+        var_dump($users);
+        die();
+
         return $view->render($response, 'home.twig');
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,11 +1,27 @@
 <?php
 
 use App\App;
+use Illuminate\Database\Capsule\Manager as Capsule;
 
 session_start();
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = new App;
+
+$capsule = new Capsule();
+$capsule->addConnection([
+    'driver' => 'pgsql',
+    'host' => 'localhost',
+    'database' => 'learn_slim',
+    'username' => 'postgres',
+    'password' => 'toor',
+    'charset' => 'utf8',
+    'collation' => 'utf_unicode_ci',
+    'prefix' => '',
+]);
+
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
 
 require __DIR__ . '/../app/routes.php';
