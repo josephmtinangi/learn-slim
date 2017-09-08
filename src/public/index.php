c12f527f05af01c975b94e559ec8703f25f360c9
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-09-08 11:56:18 +0300

    Add database connection

--- src/public/index.php
+++ src/public/index.php
@@ -24,6 +24,17 @@ $container['logger'] = function ($c) {
     return $logger;
 };
 
+$container['db'] = function ($c) {
+  $db = $c['settings']['db'];
+
+  $pdo = new PDO("mysql:host=".$db['host'].";dbnme=".$db['dbname'], $db['user'], $db['pass']);
+
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+  return $pdo;
+};
+
 $app->get('/', function (Request $request, Response $response, $args) {
     return $response->write("Hello");
 });
